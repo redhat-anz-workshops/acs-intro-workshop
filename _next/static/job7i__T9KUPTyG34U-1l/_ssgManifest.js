0b25156801135ec56f5f9bc4f8e8d85e22a7bd48
shaneboulden <1834999+shaneboulden@users.noreply.github.com>
2024-06-07 01:40:25 +0000

    Deploying to gh-pages from @ shaneboulden/acs-intro-workshop@56918be9500644443db95179e28b2fff489f6920 🚀

---
+++ _next/static/job7i__T9KUPTyG34U-1l/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Ftags","\u002Fworkshop","\u002Fabout","\u002F","\u002Fworkshop\u002Fpage\u002F[page]","\u002Fworkshop\u002F[...slug]","\u002Ftags\u002F[tag]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
