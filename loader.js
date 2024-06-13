28b983acaeec25537618fdfefff2ce892cf2d55e
shaneboulden <shane.boulden@gmail.com>
2024-06-13 20:37:06 +1000

    Fix loader syntax

--- loader.js
+++ loader.js
@@ -1,10 +1,11 @@
-export default function imgixLoader = ({ src, width, quality }) => {
+export default function imgixLoader({ src, width, quality }) {
     const url = new URL(`https://workshop-936810110.imgix.net${src}`)
-    const params = url.searchParams
     if(!src.includes("gif")) {
+        const params = url.searchParams
         params.set("auto", params.getAll("auto").join(",") || "format")
         params.set("fit", params.get("fit") || "max")
         params.set("w", params.get("w") || width.toString())
+        return url.href
     }
     return url.href
   }
