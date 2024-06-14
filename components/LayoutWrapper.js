ce58f64855f61ab382c6a0040bca98293a7e0a9a
shaneboulden <shane.boulden@gmail.com>
2024-06-14 15:57:56 +1000

    Update link formatting

--- components/LayoutWrapper.js
+++ components/LayoutWrapper.js
@@ -1,7 +1,7 @@
 import siteMetadata from '@/data/siteMetadata'
 import headerNavLinks from '@/data/headerNavLinks'
 import Logo from '@/data/logo.svg'
-import CustomLink from './Link'
+import Link from './Link'
 import SectionContainer from './SectionContainer'
 import Footer from './Footer'
 import MobileNav from './MobileNav'
@@ -13,7 +13,7 @@ const LayoutWrapper = ({ children }) => {
       <div className="flex flex-col justify-between h-screen">
         <header className="flex items-center justify-between py-10">
           <div>
-            <CustomLink href="/" aria-label="Tailwind CSS Blog">
+            <Link href="/" aria-label="Tailwind CSS Blog">
               <div className="flex items-center justify-between">
                 <div className="mr-3">
                   <Logo />
@@ -26,20 +26,23 @@ const LayoutWrapper = ({ children }) => {
                   siteMetadata.headerTitle
                 )}
               </div>
-            </CustomLink>
+            </Link>
           </div>
           <div className="flex items-center text-base leading-5">
             <div className="hidden sm:block">
               {headerNavLinks.map((link) => (
-                <CustomLink
+                <Link
                   key={link.title}
                   href={link.href}
                   className="p-1 font-medium text-gray-900 sm:p-4 dark:text-gray-100"
                 >
                   {link.title}
-                </CustomLink>
+                </Link>
               ))}
             </div>
+            <a href="https://etherpad.wikimedia.org/p/rhacs-workshop-june-2024" target="_blank" rel="noopener noreferrer" className="p-1 font-medium text-gray-900 sm:p-4 dark:text-gray-100">
+                Etherpad
+            </a>
             <ThemeSwitch />
             <MobileNav />
           </div>
--- data/headerNavLinks.js
+++ data/headerNavLinks.js
@@ -1,6 +1,5 @@
 const headerNavLinks = [
   { href: '/workshop', title: 'Exercises' },
-  { href: 'https://etherpad.wikimedia.org/p/rhacs-workshop-june-2024', title: 'Etherpad'},
 ]
 
 export default headerNavLinks
